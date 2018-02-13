4f9afdc3fc6c2723193c9572d54ff2af5d60a01c
Carlos <Chughes4life@gmail.com>
2018-02-12 20:17:21 -0500

    Added browser support fallbacks.

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -5,7 +5,7 @@ gulp.task('scripts', ['modernizr'], function(callback) {
   webpack(require('../../webpack.config.js'), function(err, stats) {
 	if (err) {
 	  console.log(err.toString());
-	}
+	} 
 	
 	console.log(stats.toString());
 	callback();
--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -8,7 +8,7 @@ var config = {
   shape: {
 	spacing: {
 	  padding: 1
-	}
+	} 
   },
   mode: { 
     css: {
